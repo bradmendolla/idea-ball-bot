d6648d3468ffaa7b5e0c41737e8e8b3e55e4ba45
bradmendolla <bradmendolla@gmail.com>
2020-01-15 16:34:56 -0500

    updated otherbot

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = 'NjY0MTg0NTE2MzIzMDQ5NDgy.Xh9t9w.23jOGKupbRtCsZ00pHbqhhsFyr8'
+GUILD = "McCain's Bot Factory"
+ROLE = "Test Subjects"
--- other_bot.py
+++ other_bot.py
@@ -1,10 +1,10 @@
 import discord
 import random
-from config import TOKEN, GUILD
+from config import TOKEN, GUILD, ROLE
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!", case_insensitive = True)
-
+role = client.Role
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} is ready and raring to go master!")
@@ -16,24 +16,22 @@ async def on_member_join(member):
     role = discord.utils.get(guild.roles, name="Test Subjects")
     await bot.member.add_roles(member, role)
 
-@bot.event
-async def on_member_leave(member):
-    await member.create_dm()
-    await member.dm_channel.send(f"You have been kicked from the Bot Factory {member.mention}. This may have been done automatically, or the Handsome one wishes to test something on you personally. An invite back to the server shall be forthcoming.")
+
 
 
 @bot.command(name="claptrap", help="Invokes Claptrap")
 async def minion(ctx):
-    clapback = ["Yes, master?", "Oh god...", "Save me minion!"]
+    clapback = ["Yes, master?", "Oh god...Stairs!", "Save me minion!"]
+
 
     response = random.choice(clapback)
     await ctx.send(response)
 
-
-@bot.command(name="ideaball")
-#define function
-async def ideaball(ctx):
-#select user
-    await discord.User
+@bot.command(name="playball", help="Summons an IdeaBall to be slammed into a random volunteer.")
+async def playball(self, ctx):
+    role = Role(name=ROLE)
+    subjects = role.members
+    selected = random.select(subjects)
+    await ctx.channel.send(f"{selected} do you want to be repeatedly shot full of questionable substances for very little test value? No response? Excellent!")
 
 bot.run(TOKEN)
