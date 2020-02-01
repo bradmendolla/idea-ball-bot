44e8f1f80786a0ec61902c46aebd9153221e8862
bradmendolla <bradmendolla@gmail.com>
2020-01-31 19:45:13 -0500

    icoBSOD

--- bot.py
+++ bot.py
@@ -10,24 +10,15 @@ bot = commands.Bot(command_prefix="!", case_insensitive = True)
 async def on_ready():
     print(f"{bot.user.name} is ready and raring to go master!")
 
-@bot.event
-async def on_message(message):
-    print(f"Author: {message.author}, #{message.channel}")
-    my_guild = bot.get_guild(GUILD)
-    
-    if message.content == "test":
-        await message.channel.send(f"Wubwubwub")
-    
-    elif message.content == "roles":
-        await print(f"{message.guild}")
 
 
 
-@bot.event
-async def on_member_join(member):
-    await member.create_dm()
-    
-    await member.dm_channel.send(f"Welcome to the Bot Factory, {member.mention}! Await your instructions from The Handsome One!")
+
+#@bot.event
+#async def on_member_join(member):
+#    await member.create_dm()
+#    
+#    await member.dm_channel.send(f"Welcome to the Bot Factory, {member.mention}! Await your instructions from The Handsome One!")
     
 
 
@@ -48,26 +39,25 @@ async def minion(ctx):
 
 @bot.command(name="playball", help="Summons an IdeaBall to be slammed into a random volunteer.")
 async def playball(ctx):
-    roles = ctx.guild.roles
-    author = ctx.author.roles
-    users = ctx.guild.members
-    online = []
-    subjects = []
+    guild = ctx.guild
+    channel = ctx.channel
+    role = get(guild.roles, name="IdeaBall")
+    users = role.members
+    ballers = []
     for user in users:
-        if user.roles[1] == roles[2]:
-            subjects.append(user)
-    
-    for subject in subjects:
-        if subject.status == online:
-            online.append(subject)
-    subject = random.choice(subjects)
-
+        if user.status == "online":
+            ballers.append(user)
+        else:
+            pass
+    baller = random.choice(ballers)
     queries = ["do you want to be repeatedly shot full of questionable substances for very little test value? No response? Excellent!", "how do you feel about hypodermic uranium tattoos?", "I'm just gonna need you to hold still while I administer this harmless carcinogen." , "needs restraints! Hold them down!", "it says you requested electro shock treatments. I'm happy to oblige!"]
     question = random.choice(queries)
-    await ctx.channel.send(f"{subject.mention} {question}")
+    #await channel.send(f"{baller.mention} {question}")
     
-    print(len(users))
-    print(len(online))
+    try:
+        await print(f"{len(ballers)} ")
+    except TypeError:
+        return
 
 
 #test code to setup a role for idea ball
@@ -75,8 +65,12 @@ async def playball(ctx):
 @bot.command()
 async def setup(ctx):
     guild = ctx.guild
-    await guild.create_role(name="IdeaBall")
-    await print(f"Setup Triggered")
+    channel = ctx.channel
+    if get(guild.roles, name="IdeaBall"):
+        await channel.send("Role Exists")
+    else:
+        await guild.create_role(name="IdeaBall")
+    
 
 #code to join role
 
@@ -85,6 +79,13 @@ async def join(ctx):
     role = get(ctx.guild.roles, name="IdeaBall")
     user = ctx.message.author
     await user.add_roles(role)
+
+#code to leave role
+@bot.command(help="Leave IdeaBall role")
+async def leave(ctx):
+    role = get(ctx.guild.roles, name="IdeaBall")
+    user = ctx.message.author
+    await user.remove_roles(role)
 
 
 bot.run(TOKEN)
